db72794ce5544d156e0b2c466206875fe737dc0f
MakineIUT <115160757+MakineIUT@users.noreply.github.com>
2024-06-03 15:26:31 +0200

    Add files via upload

---
+++ Connexion/connexion.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+
+    <head>
+        <meta charset="utf-8"/>
+        <title>Connexion</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content="Webflow" name="generator"/>
+        <link href="connexion.css" rel="stylesheet" type="text/css"/>
+        
+    </head>
+    <body>
+        <div class="_2-columns-2">
+            <div class="gauche">
+                <img src="Image.png" class="image"/>
+            </div>
+            <div class="gauche">
+                <div class="contact-form">
+                    <div class="container">
+                        <div class="section-title">
+                            <h2 class="get-in-touch">Connexion</h2>
+                        </div>
+                        <div class="form-wrapper w-form">
+                            <form id="connexion-form" name="connexion-form"  method="post" class="form-2" >
+                                <div class="input-wrapper-3">
+                                    <label for="name" class="form-block-label">Nom </label>
+                                    <input class="form-text-input-11 w-input" maxlength="256" name="name" data-name="Name" placeholder="ex : Martin Dupont" type="text" id="name"/>
+                                </div>
+                                <div class="input-wrapper-3">
+                                    <label for="name" class="form-block-label">Email</label>
+                                    <input class="form-text-input-12 w-input" maxlength="256" name="name" data-name="Name" placeholder="mon@email.com" type="text" id="name"/>
+                                </div>
+                                <input type="submit" data-wait="Please wait..." class="form-button-2 w-button" value="CONNEXION"/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
