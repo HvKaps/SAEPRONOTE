458ba11e770235177bc99c3ffa51e53a584063a6
MakineIUT <115160757+MakineIUT@users.noreply.github.com>
2024-06-03 15:27:02 +0200

    Add files via upload

---
+++ ajout de note/ajoutdenote.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>ajout de note</title>
+        <link href="ajout.css" rel="stylesheet" type="text/css"/>
+
+    </head>
+    <body>
+        <div class="ajout-de-note-prof">
+           <h3>Ajout de note</h3>
+            <div class="_2-columns">
+                <div class="column">
+                     <h4>Saisie</h4>
+                    <form id="webflow-form" name="webflow-form" data-name="Webflow Form" method="get" class="form" data-wf-page-id="665db162b51bb99730b6e7e7" data-wf-element-id="9c5d064c-c632-5b10-73e2-dc466d54571d">
+                        <div class="input-wrapper">
+                            <input class="form-text-input w-input" maxlength="256" name="name" data-name="Name" placeholder="Date évaluation" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-2 w-input" maxlength="256" name="name" data-name="Name" placeholder="Classe" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-3 w-input" maxlength="256" name="name" data-name="Name" placeholder="Type d’évaluation" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-4 w-input" maxlength="256" name="name" data-name="Name" placeholder="Ressource" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-5 w-input" maxlength="256" name="name" data-name="Name" placeholder="Étudiant" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-6 w-input" maxlength="256" name="name" data-name="Name" placeholder="Note" type="text" id="name"/>
+                        </div>
+                        <div class="boutton">
+                            <div class="column-2">
+                                <input type="submit"  class="form-button w-button" value="Valider"/>
+                            </div>
+                            <div class="column-3">
+                                <input type="submit"class="form-button w-button" value="Effacer"/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="column-4">
+
+                     <h4>Mosification</h4>
+                    <form id="webflow-form" name="webflow-form" data-name="Webflow Form" method="get" class="form" data-wf-page-id="665db162b51bb99730b6e7e7" data-wf-element-id="9c5d064c-c632-5b10-73e2-dc466d545733">
+                        <div class="input-wrapper">
+                            <input class="form-text-input w-input" maxlength="256" name="name" data-name="Name" placeholder="ID évaluation" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-7 w-input" maxlength="256" name="name" data-name="Name" placeholder="Classe" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-8 w-input" maxlength="256" name="name" data-name="Name" placeholder="Ressource" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-9 w-input" maxlength="256" name="name" data-name="Name" placeholder="Étudiant" type="text" id="name"/>
+                        </div>
+                        <div class="input-wrapper-2">
+                            <input class="form-text-input-10 w-input" maxlength="256" name="name" data-name="Name" placeholder="Note" type="text" id="name"/>
+                        </div>
+                        <div class="bouttons">
+                            <div class="column-2">
+                                <input type="submit" data-wait="Please wait..." class="form-button w-button" value="Valider"/>
+                            </div>
+                            <div class="column-3">
+                                <input type="submit" data-wait="Please wait..." class="form-button w-button" value="Effacer"/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
