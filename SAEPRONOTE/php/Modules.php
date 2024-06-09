3885b664e8c5a719e46f495c6bca5f72f896bd35
enzo37z <115540659+enzo37z@users.noreply.github.com>
2024-06-09 20:17:56 +0200

    Add files via upload

---
+++ SAEPRONOTE/php/Modules.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modules</title>
+    <link rel="stylesheet" href="Modules.css">
+</head>
+<body>
+   <header>
+        <h1>Modules</h1>
+    </header>
+    <section class="image-blocks">
+        <a href="math.php" class="block-link">
+            <div class="block">Math</div>
+        </a>
+        <a href="devweb.php" class="block-link">
+            <div class="block">Dev Web</div>
+        </a>
+        <a href="integration.php" class="block-link">
+            <div class="block">Intégration</div>
+        </a>
+        <a href="systemeinfo.php" class="block-link">
+            <div class="block">Système informatique</div>
+        </a>
+    </section>
+</body>
+</html>
